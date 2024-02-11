be17b09c621675a341ec25906b91382e192a480d
AShikShuvo <ashikshuvo1996@gmail.com>
2024-02-11 15:40:03 +0600

    auth guard added for protected route

---
+++ src/guards/auth.guard.ts
@@ -0,0 +1,23 @@
+import { CanActivate, ExecutionContext } from '@nestjs/common';
+import * as jwt from 'jsonwebtoken';
+
+export class Authenticated implements CanActivate {
+  canActivate(context: ExecutionContext): boolean {
+    const request = context.switchToHttp().getRequest();
+    const token = request.headers?.authorization?.split('Bearer ')[1];
+    if (!token) {
+      return false;
+    }
+    try {
+      return this.verifyToken(token);
+    } catch (e) {
+      return false;
+    }
+  }
+
+  private verifyToken(token: string) {
+    const payload = jwt.verify(token, process.env.JWT_TOKEN_KEY);
+
+    return !!payload;
+  }
+}
--- src/interceptors/user.interceptor.ts
+++ src/interceptors/user.interceptor.ts
@@ -1,14 +1,25 @@
 import { CallHandler, ExecutionContext, NestInterceptor } from '@nestjs/common';
+import { Observable } from 'rxjs';
 import * as jwt from 'jsonwebtoken';
 
 export class UserInterceptor implements NestInterceptor {
-  async intercept(context: ExecutionContext, next: CallHandler) {
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler,
+  ): Observable<any> | Promise<Observable<any>> {
     const request = context.switchToHttp().getRequest();
     const token = request?.headers?.authorization?.split('Bearer ')[1];
-    console.log(token);
+    if (!token) {
+      request.user = null;
+    } else {
+      const payload = jwt.verify(token, process.env.JWT_TOKEN_KEY);
+      if (!payload) {
+        request.user = null;
+      } else {
+        request.user = payload;
+      }
+    }
 
-    const user = await jwt.decode(token);
-    request.user = user;
     return next.handle();
   }
 }
