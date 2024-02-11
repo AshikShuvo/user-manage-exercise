97dd0d347889ed3ec3684aecb53993df8ae51d05
AShikShuvo <ashikshuvo1996@gmail.com>
2024-02-11 15:06:27 +0600

    interceptor and decorator added to get user basic info from token

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,18 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { UserInterceptor } from './interceptors/user.interceptor';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 
 @Module({
   imports: [UserModule, PrismaModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: UserInterceptor,
+    },
+  ],
 })
 export class AppModule {}
